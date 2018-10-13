f77c6d282bed94d5cf1fc569d800860e593d27a6
roybarak80 <barakroy@gmail.com>
2018-10-13 12:52:57 +0300

    first commit

---
+++ build/precache-manifest.5252834ec544b3f0e365b8067358ed5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "9da13ad7e70335602e7d",
+    "url": "/react-test/static/js/main.9da13ad7.chunk.js"
+  },
+  {
+    "revision": "5bd39161dfeb1bd0e015",
+    "url": "/react-test/static/js/1.5bd39161.chunk.js"
+  },
+  {
+    "revision": "9da13ad7e70335602e7d",
+    "url": "/react-test/static/css/main.7778cbdd.chunk.css"
+  },
+  {
+    "revision": "628885a7c00f2d6bea8dc2f121c66b54",
+    "url": "/react-test/index.html"
+  }
+];
